bf6d0580cbbcc67f0ec83601b8a03a3763753ab5
LuanderV <luander.vargas@gmail.com>
2024-08-07 17:38:56 -0300

    feat: push

---
+++ src/components/Equipes.jsx
@@ -0,0 +1,16 @@
+function Equipes() {
+    return (
+        <>
+            <main className="m-5 p-5 border">
+                <h1 className="m-5 p-5 border">Equipes</h1>
+                <section className="m-5 p-5 border">
+                    <div className="m-5 p-5 border">
+                        <p>Equipe</p>
+                    </div>
+                </section>
+            </main>
+        </>
+    )
+}
+
+export default Equipes;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <>
+            <footer className="bg-emerald-200 text-white p-4 text-center w-full fixed bottom-0">
+                <p>&copy; 2024 Meu Site. Todos os direitos reservados.</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
